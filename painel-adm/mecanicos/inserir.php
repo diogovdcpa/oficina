291a316b967217e2828ef65a7643c4c8f9122316
Diogo Caldas <diogovdcpa@gmail.com>
2021-03-02 13:59:43 -0300

    metodo para inserir dados

--- painel-adm/mecanicos/inserir.php
+++ painel-adm/mecanicos/inserir.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('../../conexao.php');
+
 $nome = $_POST['nome_mec'];
 $cpf = $_POST['cpf_mec'];
 $telefone = $_POST['telefone_mec'];
@@ -8,6 +10,11 @@ $endereco = $_POST['endereco_mec'];
 
 $query = $pdo->query("SELECT * FROM mecanicos where cpf = '$cpf' ");
 $res = $query->fetchAll(PDO::FETCH_ASSOC);
-
 $total_reg = @count($res);
 
+if($total_reg > 0){
+    echo "CPF ja existe";
+    exit();
+}
+
+$query = $pdo->prepare("INSERT INTO mecanicos SET nome = :nome, cpf = :cpf, nome = :nome, telefone = :telefone, email = :email, endereco = :endereco");
